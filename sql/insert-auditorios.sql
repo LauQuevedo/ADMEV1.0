767e86d2cc13c3098187d57ff75974eec2748b46
edgardodth <edgardodth@gmail.com>
2018-10-29 22:54:45 -0600

    creating sql script to add auditorios

---
+++ sql/insert-auditorios.sql
@@ -0,0 +1,4 @@
+INSERT INTO auditorio VALUES (1, "Matute Remus", 50, 250),
+                             (2, "Enrique Diaz", 35, 200);
+                             (3, "Antonio Rodriguez", 30, 150);
+                             (4, "Antonio Alatorre", 30, 160);
