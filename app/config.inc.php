6a5d3ed02758926b5035bc8b5ab2b3b74b9368c1
edgardodth <edgardodth@gmail.com>
2018-10-31 19:19:10 -0600

    change extension of login file to php

---
+++ app/config.inc.php
@@ -0,0 +1,9 @@
+<?php
+
+define('NOMBRE_SERVIDOR', 'localhost');
+define('NOMBRE_USUARIO', 'root');
+define('PASSWORD', '1234');
+define('NOMBRE_DB', 'admev');
+
+define('SERVIDOR', 'http://localhost/admev');
+define('RUTA_LOGIN', SERVIDOR.'/template/login/login.php');
