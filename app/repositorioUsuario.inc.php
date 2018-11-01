05fd995a929da4850b62f5cfa8aa3d0db7bffbd9
edgardodth <edgardodth@gmail.com>
2018-10-31 19:44:03 -0600

    creating repo for class user

---
+++ app/repositorioUsuario.inc.php
@@ -0,0 +1,29 @@
+<?php
+
+class RepositorioUsuario {
+
+    public static function insertarUsuario($conexion, $usuario) {
+        $usuario_insertado = false;
+
+        if(isset($conexion)) {
+            try {
+                $sql = "INSERT INTO usuarios(nombre, email, pass, fecha_registro, activo) VALUES(:nombre, )";
+
+                $sentencia = $conexion->prepare($sql);
+                $usuario_nombre =  $usuario->getNombre();
+                $usuario_email =  $usuario->getEmail();
+                $usuario_pass =  $usuario->getPass();
+                $sentencia->bindParam(':nombre', $usuario_nombre, PDO::PARAM_STR);
+                $sentencia->bindParam(':email', $usuario_email, PDO::PARAM_STR);
+                $sentencia->bindParam(':pass', $usuario_pass, PDO::PARAM_STR);
+                $usuario_insertado = $sentencia->execute();
+
+            } catch (PDOException $ex) {
+                print("ERROR: ".$ex->getMessage());
+            }
+        }
+        return $usuario_insertado;
+    }
+}
+
+?>
