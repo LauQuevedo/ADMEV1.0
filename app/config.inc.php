a0ff1c4fdd95e1ba8315234ebf6cd50a49572cb5
edgardodth <edgardodth@gmail.com>
2018-11-28 21:54:50 -0600

    updating server link

--- app/config.inc.php
+++ app/config.inc.php
@@ -5,5 +5,5 @@ define('NOMBRE_USUARIO', 'root');
 define('PASSWORD', '1234');
 define('NOMBRE_DB', 'admev');
 
-define('SERVIDOR', 'http://localhost/ADMEV1.0/templates/landing-page/principal.html');
+define('SERVIDOR', 'http://localhost/ADMEV1.0/index.php');
 define('RUTA_LOGIN', SERVIDOR.'/template/login/login.php');
